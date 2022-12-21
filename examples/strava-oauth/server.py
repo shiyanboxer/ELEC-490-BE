430bda67869f92c88bab4795a648a5ae4f20ace3
Shiyan Boxer <shiyanboxer7@gmail.com>
2022-12-21 16:40:20 -0500

    Ground work for predict API

--- examples/strava-oauth/server.py
+++ examples/strava-oauth/server.py
@@ -94,9 +94,40 @@ def get_activity():
     return
 
 
-@app.route("/predict")
-def predict():
-    return
+# def load_models():
+#     """"
+#     Take the pickled model file, and open and load it into a variable called "model" 
+#     Return: "model", an object of our model
+#     """
+#     file_name = "2Model/model_file.p"
+#     with open(file_name, 'rb') as pickled:
+#         data = pickle.load(pickled)
+#         model = data['model']
+#     return model  
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     """
+#     Predict the success of a company from the user's input
+#     Return: "response" which is a value either 1 or 0.
+#     """
+
+#     model = load_models() # Get an instance of the model calling the load_models()
+#     data = json.loads(request.data) # Load the request from the user and store in the variable "data"
+
+#     # Raises a 400 error if invalid input
+#     if type(data['total_funding']) != int or type(data['founded_year']) != int:
+#         return 'User entered invalid input type', 400
+
+#     total_funding = float(data['total_funding']) # Break down each input into seprate variables 
+#     founded_year = float(data['founded_year'])
+    
+#     X_test = np.array([total_funding, founded_year]) # Create a X_test variable of the user's input
+#     prediction = model.predict(X_test.reshape(1, -1)) # Use the the  X_test to to predict the success using the  predict()
+#     response = json.dumps({'response': np.around(prediction[0], 0)}) # Dump the result to be sent back to the frontend
+    
+#     return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
