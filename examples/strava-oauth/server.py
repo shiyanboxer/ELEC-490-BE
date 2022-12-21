41917697cf820ec63ba813035a66324d8a0d3f8a
Shiyan Boxer <shiyanboxer7@gmail.com>
2022-12-21 16:22:03 -0500

    Extract stream but not heart rate data

--- examples/strava-oauth/server.py
+++ examples/strava-oauth/server.py
@@ -1,3 +1,5 @@
+# Strava tokens https://www.strava.com/settings/api
+
 #!flask/bin/python
 import logging
 
@@ -19,12 +21,12 @@ STRAVA_CLIENT_SECRET= os.getenv('STRAVA_CLIENT_SECRET')
 
 @app.route("/")
 def login():
-    c = Client()
+    client = Client()
     # url = c.authorization_url(client_id=app.config['STRAVA_CLIENT_ID'],
     #                           redirect_uri=url_for('.logged_in', _external=True),
     #                           approval_prompt='auto')
 
-    url = c.authorization_url(
+    url = client.authorization_url(
         client_id=STRAVA_CLIENT_ID,
         redirect_uri=url_for('.logged_in',
         _external=True),
@@ -54,9 +56,40 @@ def logged_in():
         
         # Probably here you'd want to store this somewhere -- e.g. in a database.
         strava_athlete = client.get_athlete()
+        
+        types = ['time', 'heartrate', 'temp']
+        
+         # Get all activity
+         # https://developers.strava.com/docs/reference/#api-Activities-getLoggedInAthleteActivities
+        for activity in client.get_activities(after = "2010-01-01T00:00:00Z",  limit=5):
+            print("{0.name} {0.moving_time}".format(activity))
+            
+            # Activities can have many streams, you can request n desired stream types
+            stream = client.get_activity_streams(activity.id, types=types, resolution='medium')
 
+            # Result is a dictionary object.  The dict's key are the stream type.
+            if 'heartrate' in stream.keys():
+                print(stream['heartrate'].data)
+            
         return render_template('login_results.html', athlete=strava_athlete, access_token=access_token)
 
+
+@app.route("/get_activity")
+def get_activity():
+    client = Client()
+    
+    # Get all activity
+    for activity in client.get_activities(after = "2010-01-01T00:00:00Z",  limit=5):
+        print("{0.name} {0.moving_time}".format(activity))
+        
+        # Activities can have many streams, you can request n desired stream types
+        stream = client.get_activity_streams(activity.id, types=types, resolution='medium')
+
+        # Result is a dictionary object.  The dict's key are the stream type.
+        if 'heartrate' in stream.keys():
+            print(stream['heartrate'].data)
+            
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
