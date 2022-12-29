6d6c7830e73f3f315e20b55efe2440e054da52ee
Shiyan Boxer <shiyanboxer7@gmail.com>
2022-12-29 11:36:50 -0500

    Adding model files

--- examples/strava-oauth/server.py
+++ examples/strava-oauth/server.py
@@ -4,16 +4,17 @@
 import logging
 
 from flask import Flask, render_template, redirect, url_for, request, jsonify
-
-# Enviroment variables 
-# https://dev.to/jakewitcher/using-env-files-for-environment-variables-in-python-applications-55a1
+import pickle
+import json
+# import sklearn as sklearn
+import numpy as np
+# Enviroment variables - https://dev.to/jakewitcher/using-env-files-for-environment-variables-in-python-applications-55a1
 import os
 from dotenv import load_dotenv
-
 from stravalib import Client
 
 app = Flask(__name__)
-# app.config.from_envvar('APP_SETTINGS')
+
 load_dotenv()
 
 STRAVA_CLIENT_ID = os.getenv('STRAVA_CLIENT_ID')
@@ -22,9 +23,6 @@ STRAVA_CLIENT_SECRET= os.getenv('STRAVA_CLIENT_SECRET')
 @app.route("/")
 def login():
     client = Client()
-    # url = c.authorization_url(client_id=app.config['STRAVA_CLIENT_ID'],
-    #                           redirect_uri=url_for('.logged_in', _external=True),
-    #                           approval_prompt='auto')
 
     url = client.authorization_url(
         client_id=STRAVA_CLIENT_ID,
@@ -57,34 +55,32 @@ def logged_in():
         # Probably here you'd want to store this somewhere -- e.g. in a database.
         strava_athlete = client.get_athlete()
         
-        types = ['time', 'latlng', 'distance', 'altitude', 'velocity_smooth', 
-        'heartrate', 'cadence', 'watts', 'temp', 'moving', 'grade_smooth']
-
-         # Get all activity
-         # https://developers.strava.com/docs/reference/#api-Activities-getLoggedInAthleteActivities
-        #  To get activities in oldest to newest, specify a value for the after argument. To get newest to oldest use before argument.
-        for activity in client.get_activities(before = "2030-01-01T00:00:00Z",  limit=10):
-            print("{0.name} {0.moving_time}".format(activity))
-            
-            # Activities can have many streams, you can request n desired stream types
-            stream = client.get_activity_streams(activity.id, types=types, resolution='medium')
-
-            # Result is a dictionary object.  The dict's key are the stream type.
-            if 'heartrate' in stream.keys():
-                print(stream['heartrate'].data)
-            
         return render_template('login_results.html', athlete=strava_athlete, access_token=access_token)
 
 
-@app.route("/activity")
-def get_activity():
+def load_models():
+    """"
+    Take the pickled model file, and open and load it into a variable called "model" 
+    Return: "model", an object of our model
+    """
+    file_name = "examples/strava-oauth/model_file.p"
+    with open(file_name, 'rb') as pickled:
+        data = pickle.load(pickled)
+        model = data['model']
+    return model  
+
+
+@app.route("/predict")
+def predict():
     client = Client()
     
     types = ['time', 'latlng', 'distance', 'altitude', 'velocity_smooth', 
     'heartrate', 'cadence', 'watts', 'temp', 'moving', 'grade_smooth']
 
     # Get all activity
-    for activity in client.get_activities(after = "2010-01-01T00:00:00Z",  limit=5):
+    # https://developers.strava.com/docs/reference/#api-Activities-getLoggedInAthleteActivities
+    # To get activities in oldest to newest, specify a value for the after argument. To get newest to oldest use before argument.
+    for activity in client.get_activities(after = "2010-01-01T00:00:00Z",  limit=10):
         print("{0.name} {0.moving_time}".format(activity))
         
         # Activities can have many streams, you can request n desired stream types
@@ -93,44 +89,22 @@ def get_activity():
         # Result is a dictionary object.  The dict's key are the stream type.
         if 'heartrate' in stream.keys():
             print(stream['heartrate'].data)
-            
-    return
-
-
-# def load_models():
-#     """"
-#     Take the pickled model file, and open and load it into a variable called "model" 
-#     Return: "model", an object of our model
-#     """
-#     file_name = "2Model/model_file.p"
-#     with open(file_name, 'rb') as pickled:
-#         data = pickle.load(pickled)
-#         model = data['model']
-#     return model  
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     """
-#     Predict the success of a company from the user's input
-#     Return: "response" which is a value either 1 or 0.
-#     """
-
-#     model = load_models() # Get an instance of the model calling the load_models()
-#     data = json.loads(request.data) # Load the request from the user and store in the variable "data"
-
-#     # Raises a 400 error if invalid input
-#     if type(data['total_funding']) != int or type(data['founded_year']) != int:
-#         return 'User entered invalid input type', 400
-
-#     total_funding = float(data['total_funding']) # Break down each input into seprate variables 
-#     founded_year = float(data['founded_year'])
     
-#     X_test = np.array([total_funding, founded_year]) # Create a X_test variable of the user's input
-#     prediction = model.predict(X_test.reshape(1, -1)) # Use the the  X_test to to predict the success using the  predict()
-#     response = json.dumps({'response': np.around(prediction[0], 0)}) # Dump the result to be sent back to the frontend
     
-#     return response
+    model = load_models() # Get an instance of the model calling the load_models()
+    data = json.loads(request.data) # Load the request from the user and store in the variable "data"
 
+    # Raises a 400 error if invalid input
+    if type(data['hrv_value']) != int:
+        return 'User entered invalid input type', 400
+
+    hrv_value = float(data['hrv_value']) # Break down each input into seprate variables 
+    
+    x_test = np.array([hrv_value]) # Create a X_test variable of the user's input
+    prediction = model.predict(x_test.reshape(1, -1)) # Use the the  X_test to to predict the success using the  predict()
+    response = json.dumps({'response': np.around(prediction[0], 0)}) # Dump the result to be sent back to the frontend
+    
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ examples/strava-oauth/server_test.py
@@ -0,0 +1,80 @@
+import requests
+from server import server
+import flask.app as app
+import unittest
+from flask import Flask
+import json
+
+# https://docs.python.org/3/library/unittest.html
+# https://www.w3schools.com/python/ref_requests_response.asp
+# https://www.youtube.com/watch?v=iQVvpnRYl-w
+# https://www.geeksforgeeks.org/get-post-requests-using-python/
+# https://www.ontestautomation.com/writing-tests-for-restful-apis-in-python-using-requests-part-1-basic-tests/
+
+API_URL_HELLO = "http://127.0.0.1:5000/"
+API_URL_PREDICT = "http://127.0.0.1:5000/predict"
+
+class TestCase(unittest.TestCase):
+    # HELLO - status code - 200 (ok)
+    def test_hello_status(self):
+        response = requests.get(API_URL_HELLO)
+        self.assertEqual(response.status_code, 200)
+    
+    # HELLO - checking request url
+    def test_url(self):
+        response = requests.get(API_URL_HELLO)
+        self.assertEqual(response.url, API_URL_HELLO)
+
+    # HELLO - request type
+    def test_hello_status(self):
+        response = requests.get(API_URL_HELLO)
+        self.assertTrue(str(response.request), "[GET]")
+
+    # HELLO - read contents type
+    def test_hello_readcontent(self):
+        response = requests.get(API_URL_HELLO)
+        self.assertEqual(response.headers["Content-Type"], "text/html; charset=utf-8")
+    
+    # # HELLO - check for data returned
+    def test_read_content(self):
+        expectedResult = "hello world"
+        response = requests.get(API_URL_HELLO)
+        self.assertEqual(response.text, expectedResult)
+    
+    # -------------------------------------------------
+
+    # # PREDICT - status code - 200 (ok) with parameters
+    def test_predict_successful(self):
+        parameter = {"total_funding": 100, "founded_year": 100}
+        response = requests.post(API_URL_PREDICT, json = parameter)
+        self.assertEqual(response.status_code, 200)
+
+    # # PREDICT - method not allowed without parameters
+    def test_predict_no_param(self):
+        response = requests.post(API_URL_PREDICT)
+        self.assertEqual(response.status_code, 500)
+      
+    # # PREDICT - check status code if only one paramters is passed in
+    def test_predict_one_param(self):
+        parameter_1 = {"total_funding": 100}
+        parameter_2 = {"founded_year": 100}
+        response_1 = requests.post(API_URL_PREDICT, json = parameter_1)
+        response_2 = requests.post(API_URL_PREDICT, json = parameter_2)
+        self.assertEqual(response_1.status_code, 500)
+        self.assertEqual(response_2.status_code, 500)
+
+    # # PREDICT - check status code if invalid parameter type is passed
+    def test_predict_invalid_input(self):
+        parameter = {"total_funding": 100, "founded_year": "one thousand"}
+        response = requests.post(API_URL_PREDICT, json = parameter)
+        self.assertEqual(response.status_code, 400)
+
+    # # PREDICT - check response
+    def test_predict_successful(self):
+        parameter = {"total_funding": 100, "founded_year": 100}
+        response = requests.post(API_URL_PREDICT, json = parameter)
+        expected_result = "1.0"
+        self.assertIn(expected_result, response.text)
+
+if __name__ == '__main__':
+    unittest.main()
